1bf10268666831a466b8aada52e06db0edea47e1
E-Lop <edmondolopez@gmail.com>
2022-06-09 15:13:23 +0200

    esercizio completato

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,2 +1,16 @@
 /*  Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti. */
+
+// variabile flag per contenere la somma
+let somma = 0;
+
+// ciclo di 10 richieste di numeri all'utente
+for (let i = 0; i < 10; i++) {
+  // inserisco il dato dell'utente nella variabile come numero
+  let userNumber = parseInt(prompt('Scrivi un numero'));
+  //   eseguo la somma alla variabile flag ogni iterazione
+  somma += userNumber;
+}
+
+// stampa della sommatoria al termine del ciclo
+console.log(somma);
