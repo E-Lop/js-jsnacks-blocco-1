829919b1ef18144e2c87cd778084eb97ecd865b1
E-Lop <edmondolopez@gmail.com>
2022-06-10 10:30:08 +0200

    correzione const e diverso da

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,7 +3,7 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
 
 // creazione array vuoto
-let oddNumbers = [];
+const oddNumbers = [];
 
 // dichiarazione variabile per input utente
 let userNumber;
@@ -13,7 +13,7 @@ for (let i = 0; i < 6; i++) {
   // richiesta di numero
   userNumber = parseInt(prompt('Dimmi un numero'));
   //   se il numero è dispari
-  if (userNumber % 2 != 0) {
+  if (userNumber % 2 !== 0) {
     //   inserisci il numero corrente nell'array
     oddNumbers.push(userNumber);
   }
